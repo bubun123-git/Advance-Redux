2276e9a6080c7c21578ab5c12f6eb2cac70085f4
bubun123-git <bubundcool@gmail.com>
2023-10-17 00:07:09 +0530

    Add to Cart Fixes

--- src/Store/cart-slice.js
+++ src/Store/cart-slice.js
@@ -9,15 +9,15 @@ const cartSlice = createSlice({
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
-      const existingItem = state.items.find(item => item.itemID === newItem.itemID);
+      const existingItem = state.items.find(item => item.id === newItem.id);
       state.totalQuantity++;
       if (!existingItem) {
         state.items.push({
-          id: newItem.itemID,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price,
-          name: newItem.name
+          name: newItem.name,
         });
       } else {
         existingItem.quantity++;
@@ -27,10 +27,10 @@ const cartSlice = createSlice({
 
     removeItemFromCart(state, action) {
       const id = action.payload;
-      const existingItem = state.items.find(item => item.itemID === id);
+      const existingItem = state.items.find(item => item.id === id);
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
-        state.items = state.items.filter(item => item.itemID !== id);
+        state.items = state.items.filter(item => item.id !== id);
       } else {
         existingItem.quantity--;
         existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
